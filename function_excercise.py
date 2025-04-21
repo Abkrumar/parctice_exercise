0584ff5feaec1db0da9b40ea0812669800888952
Abkrumar <abubakarumaralkanawy@gmail.com>
2025-04-21 06:31:09 -0700

    funtion exercises

---
+++ function_excercise.py
@@ -0,0 +1,52 @@
+# # Question 1:Write a function called display_message() that prints one sentence
+# telling everyone what you are learning about in this chapter. Call the
+# function, and make sure the message displays correctly.
+
+
+def display_message(message):
+    print('''Hello everyone! In this chapter I am  learning a variety of ways to pass information to functions.
+I am also learning how to write certain functions whose primary job is to display information and 
+other functions designed to process data and return a value or set of values.Finally, I am learning to how 
+store functions in separate files called modules to help organize your main program files''')
+
+
+display_message("good")
+
+
+# quetion 2: Write a function called favorite_book() that accepts one
+# parameter, title. The function should print a message, such as One of my
+# favorite books is Alice in Wonderland. Call the function, making sure to
+# include a book title as an argument in the function call.
+
+
+def favourite_book(Math, biology):
+    print(" My favorite books is {math}and {bilogy}.")
+
+
+favourite_book("calxulus", "genetics")
+
+# # uestion 3:
+# # """ T-Shirt: Write a function called make_shirt() that accepts a si
+# ze and the text of a message that should be printed on the shirt.
+#  The function should print a sentence summarizing the
+#  size of the shirt and the message printed on it.
+# Call the function once using positional arguments to make a
+# shirt. Call the function a second time using keyword arguments"""
+
+
+def T_shirt(Size, message):
+    print(f'the size of the T-shirt is {Size} ')
+    print(f"message displayed is {message}")
+
+
+T_shirt("35", "Better by far")
+T_shirt(Size="35", message="Better by far")
+
+# using return
+
+def retur(good, bad):
+    """retun full neatly"""
+    do= f"{good}and {bad}"
+    return do
+message= ("charity", "stealing")
+print(message)
